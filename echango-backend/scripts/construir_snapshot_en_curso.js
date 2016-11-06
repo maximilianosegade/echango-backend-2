6dd326c2808fd92ca4c93309f66f3e57a311e2e9
Maximiliano Segade <maximilianosegade@gmail.com>
2016-11-06 17:44:26 -0300

    Snapshot en curso

---
+++ echango-backend/scripts/construir_snapshot_en_curso.js
@@ -0,0 +1,196 @@
+var mongoUrl = 'mongodb://echango_mobile:aprobamos el 22/11@ec2-35-162-193-58.us-west-2.compute.amazonaws.com:38128/echango';
+
+function obtenerComprasHistoricoPorId(id){
+	var dbTrack;
+
+	console.log('Se van a buscar compras historicas por ID [', id, ']...');
+	
+	return new Promise(function(resolve, reject){
+
+		var MongoClient = require('mongodb').MongoClient;	
+		MongoClient.connect(mongoUrl, function(err, db) {
+			dbTrack = db;
+
+			if (err){
+				if (dbTrack){
+					dbTrack.close();
+				}
+				console.log('Error connect', err);
+				reject(err);
+			}
+			
+			console.log('Conexion a DB exitosa.');
+			console.log('Buscar en tabla compras_hist...');
+
+		    db.collection('compras_hist').find({'_id': id}).toArray(function(err, result){
+		    	if (err){
+		    		if (dbTrack){
+						dbTrack.close();
+					}
+		    		console.log('Error insert', err);
+		    		reject(err);
+		    	}
+
+		    	if (dbTrack){
+					dbTrack.close();
+				}
+
+				if (!result.length)
+					reject(new Error('No se encontro el documento con las compras historicas.'))
+				else
+					resolve(result[0].data);
+		    });
+		});
+
+	});
+
+}
+
+
+var id= '2016-11-06 00:59:12';
+var snapComercios = {};
+var usuarios = [];
+var comercios = [];
+
+// obtener compras hist.
+obtenerComprasHistoricoPorId(id).then(function(compras){
+
+	var i,
+		j,
+		idComercio,
+		ean,
+		user,
+		precio;
+
+	for (i=0; i<compras.length; i++){
+		idComercio = compras[i].comercio;
+		user = compras[i].usuario;
+		// Guardo lista de usuarios y comercios afectados para usarlos
+		// cuando termino de generar snapComercios.
+		usuarios.push(user);
+		comercios.push(idComercio);
+
+		// Si no existe el comercio lo creo.
+		if (!snapComercios[idComercio]){
+			snapComercios[idComercio] = {
+				_id: idComercio
+			}
+		}
+
+		for (j=0; j<compras[i].detalle_compra.articulos.length; j++){
+			ean = compras[i].detalle_compra.articulos[j].ean;
+			precio = compras[i].detalle_compra.articulos[j].precio_lista;
+
+			// Si no tiene guardado el EAN lo creo.
+			if (!snapComercios[idComercio][ean]){
+				snapComercios[idComercio][ean] = {
+					novedades: []
+				}
+			}
+
+			snapComercios[idComercio][ean].novedades.push({
+				usuario: user,
+				precio: precio
+			});
+		}
+
+	}
+
+	usuarios = require('underscore').uniq(usuarios);
+	comercios = require('underscore').uniq(comercios);
+	console.log('Usuarios de novedades: ', require('underscore').uniq(usuarios));
+	console.log('Comercios de novedades: ', require('underscore').uniq(comercios));
+
+	return new Promise(function(resolve, reject){
+
+		var MongoClient = require('mongodb').MongoClient;	
+		MongoClient.connect(mongoUrl, function(err, db) {
+			dbTrack = db;
+
+			if (err){
+				if (dbTrack){
+					dbTrack.close();
+				}
+				console.log('Error connect', err);
+				reject(err);
+			}
+			
+			console.log('Conexion a DB exitosa.');
+			console.log('Buscar en tabla usuarios...');
+
+		    db.collection('usuarios').find({'_id': {'$in': usuarios}},{_id:1, score:1}).toArray(function(err, result){
+		    	if (err){
+		    		if (dbTrack){
+						dbTrack.close();
+					}
+		    		console.log('Error insert', err);
+		    		reject(err);
+		    	}
+
+		    	if (dbTrack){
+					dbTrack.close();
+				}
+
+				resolve(result);
+		    });
+
+		});
+
+	});
+
+}).then(function(scores){
+
+	console.log('Scores obtenidos: ', scores);
+
+	return new Promise(function(resolve, reject){
+
+		var MongoClient = require('mongodb').MongoClient;	
+		MongoClient.connect(mongoUrl, function(err, db) {
+			dbTrack = db;
+
+			if (err){
+				if (dbTrack){
+					dbTrack.close();
+				}
+				console.log('Error connect', err);
+				reject(err);
+			}
+			
+			console.log('Conexion a DB exitosa.');
+			console.log('Buscar en tabla ultima_snapshot...');
+
+		    db.collection('ultima_snapshot').find({'_id': {'$in': usuarios}, 'precios': {}},{_id:1, score:1}).toArray(function(err, result){
+		    	if (err){
+		    		if (dbTrack){
+						dbTrack.close();
+					}
+		    		console.log('Error insert', err);
+		    		reject(err);
+		    	}
+
+		    	if (dbTrack){
+					dbTrack.close();
+				}
+
+				resolve(result);
+		    });
+
+		});
+
+	});
+
+
+}).catch(function(err){
+
+	console.log('Fallo la generacion de snapshot en curso para [', id, ']. ', err);
+
+})
+
+// por cada comercio:
+
+	// armar snap base por comercio.
+
+	// actualizar precios base segun ultimo snapshot.
+
+	// actualizar score users segun tabla usuarios.
+
