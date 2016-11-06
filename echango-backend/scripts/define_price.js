684c2dd32202e673f3898259a4aa7aadd0e0ac12
Maximiliano Bracho <maximiliano.bracho@outlook.com.ar>
2016-11-06 11:50:06 -0300

    nuevo define_precios

--- echango-backend/scripts/define_price.js
+++ echango-backend/scripts/define_price.js
@@ -2,6 +2,8 @@ var https = require('https');
 var util = require('util');
 var MongoClient = require('mongodb').MongoClient;	
 var mongoUrl = 'mongodb://echango_mobile:aprobamos el 22/11@ec2-35-162-193-58.us-west-2.compute.amazonaws.com:38128/echango';
+var ean = "7790895000997";
+var precio_base_weight = 1000;
 
 // Obtener novedades de CouchDB (novedades_subida)
 var fechaHoy = new Date().toISOString()
@@ -24,9 +26,9 @@ get_connect.then(function(db){
 
 	var define_price = new Promise(function(resolve,reject){
 
-		var cursor = db.collection('snapshot_en_curso').find()
+		var mySnap = db.collection('snapshot_en_curso').find()
 		console.log("Devuelvo conexión a DB y cursor")
-		resolve(cursor);
+		resolve(mySnap);
 	})
 
 	define_price.then(function(mySnap){
@@ -45,22 +47,93 @@ get_connect.then(function(db){
 				console.log("No existen registros por precesar.")
 				console.log("Se han procesado las novedades y generado el último snapshot.")
 				console.log("Cerrando conexión a base de datos...")
-				db.close()
 				return Promise.resolve("0");
 
 			} else {
 
-			console.log("Imprimo el comercio")
-			console.log(comercio)
-			console.log(comercio.novedades)
-		   	var com= JSON.stringify(comercio)
-		   	console.log(com)
-		   	console.log(JSON.parse(com))
-		   	return Promise.resolve(comercio);
+				console.log("EAN:",ean)
+				console.log("Registro:")
+				console.log(comercio)
 
+				precio_base = comercio[ean].precio_base
+				console.log("Precio base: $",precio_base)
+
+				var novedades = comercio[ean].novedades
+				console.log("Imprimo novedades del artículo - Tipo:",typeof comercio)
+				console.log(novedades)
+
+				precio_novedad_num = precio_base * precio_base_weight
+				console.log("precio_novedad_num = ",precio_novedad_num)
+				precio_novedad_denom = precio_base_weight
+				console.log("precio_novedad_denom = ",precio_novedad_denom)
+
+				for (i=0; i<novedades.length; i++){
+					novedad_nro = i+1
+
+					var get_user_weight = new Promise(function(resolve,reject){
+
+						var cursor = db.collection('usuarios')
+						console.log("Devuelvo usuario")
+						
+						cursor.findOne({_id:novedades[i].usuario},function(err,usuario){
+							if (err)
+								reject(err)
+
+							console.log("Se obtuvo el usuario:", usuario)
+							resolve(usuario);
+						})						
+					});
+
+					get_user_weight.then(function(usuario){
+
+						console.log("Usuario:",usuario)
+						peso_novedad = usuario.score
+
+						resolve(peso_novedad)
+					}.then(function(peso_novedad){
+						console.log("Precio de novedad N°",novedad_nro,"= $",novedades[i].precio)
+						console.log("Peso de novedad N°",novedad_nro,"= ",peso_novedad)
+
+						console.log("precio_novedad_num ANTES:",precio_novedad_num)
+						precio_novedad_num += novedades[i].precio * peso_novedad
+						console.log("precio_novedad_num DESPUES:",precio_novedad_num)
+						
+						console.log("precio_novedad_denom ANTES:",precio_novedad_denom)
+						precio_novedad_denom += peso_novedad
+						console.log("precio_novedad_denom DESPUES:",precio_novedad_denom)
+					}))
+				}
+				var precio_novedad = precio_novedad_num / precio_novedad_denom
+				console.log("Precio novedad: $",precio_novedad)
+
+				db.close()
+
+				//NO BORRAR ----
+				/*var keys = Object.keys(comercio)
+				console.log("Imprimo las keys de comercio")
+				console.log(keys)
+				
+				var articulo = comercio.ean
+				console.log("Imprimo el articulo -  Tipo:", typeof articulo)
+				console.log(articulo)
+
+				var comercio1 = JSON.stringify(comercio,null,3)
+				console.log("Imprimo el comercio con JSON.stringify - Tipo:", typeof comercio1)
+			   	console.log(comercio1)
+
+			   	var comercio2 = util.inspect(comercio,false,null)
+				console.log("Imprimo el comercio con util.inspect - Tipo:", typeof comercio2)
+			   	console.log(comercio2)
+
+				var comercio3 = JSON.parse(comercio1)
+			   	console.log("Imprimo el comercio con JSON.parse - Tipo:",typeof comercio3)
+			   	console.log(comercio3)
+				*/
+				//NO BORRAR ----
+
+		   		return Promise.resolve(precio_novedad);
 		   }
 		})
-
 	}).catch(function(err){
 
 		console.log(err)
