5d2b3cff0dbf8b38d79c6aeda717feb0c8a1b47d
Maximiliano Segade <maximilianosegade@gmail.com>
2016-11-12 17:35:59 -0300

    Agrego user_score_management, actualzar_snap y backup

---
+++ echango-backend/scripts/script_final/40_actualizar_ultimo_snapshot.js
@@ -0,0 +1,80 @@
+var dbutils = require('../../util/mongo');
+
+module.exports = {
+
+	actualizarUltimaSnapshot: function(){
+			
+		return obtenerNovedadesPrecios().then(function(novedades){
+			return impactarNovedades(novedades);
+		});
+		
+	}
+}
+
+function obtenerNovedadesPrecios(){
+	
+	console.log('Se van a buscar las novedades del snapshot ' +
+		'en curso...');
+	
+	return dbutils.find('snapshot_en_curso', {}).then(function(resp){
+		console.log('Se obtuvieron las novedades:');
+		console.log(resp);
+		return Promise.resolve(resp);
+	});
+
+}
+
+function impactarNovedades(novedades){
+
+	var updatesNovedades = [];
+
+	console.log('Se van a impactar las novedades en el ultimo snapshot de precios...');
+
+	for (var i=0; i<novedades.length; i++){
+		novedadesComercio = novedades[i];
+
+		var update = dbutils.find('ultima_snapshot', {
+			_id: novedadesComercio._id
+		}).then(function(resp){
+
+			var ultimoSnapshotComercio = resp[0];
+			var precioOld;
+			var modificado = false;
+
+			for (ean in novedadesComercio){
+
+				if ( !(ean === '_id') ){
+					modificado = true;
+
+					precioOld = ultimoSnapshotComercio.precios[ean];
+					ultimoSnapshotComercio.precios[ean] =
+						novedadesComercio[ean].precio_novedad;					
+
+					console.log('Se actualizo ean: ', ean,
+						' en comercio: ', novedadesComercio._id,
+						' precio: ', precioOld, ' => ',
+						ultimoSnapshotComercio.precios[ean]);
+				}
+
+			}
+
+			if (modificado){
+
+				console.log('Se va a actualizar comercio: ', novedadesComercio._id, '.');
+
+				return dbutils.update('ultima_snapshot', {
+					_id: novedadesComercio._id
+				}, ultimoSnapshotComercio);
+
+			}else{
+				return Promise.resolve();
+			}
+
+		});
+
+		updatesNovedades.push(update);
+
+	}
+
+	return Promise.all(updatesNovedades);	
+}
---
+++ echango-backend/scripts/script_final/50_backup_snapshot_en_curso.js
@@ -0,0 +1,54 @@
+var dbutils = require('../../util/mongo');
+
+module.exports = {
+	backUp: function (id){
+		return obtenerSnapshotEnCurso().then(function(snap){
+			return backupSnapshotEnHistorico(id, snap);
+		});
+	}
+}
+
+function obtenerSnapshotEnCurso(){
+	
+	console.log('Se van a buscar las novedades del snapshot ' +
+		'en curso...');
+	
+	return dbutils.find('snapshot_en_curso', {}).then(function(resp){
+		console.log('Se obtuvieron las novedades:');
+		console.log(resp);
+		return Promise.resolve(resp);
+	});
+
+}
+
+function backupSnapshotEnHistorico(idSnapshot, snapshot){
+
+	var periodo = idSnapshot.substring(0,7);
+	var inserts = [];
+
+	console.log('Se va a backupear el snapshot :', idSnapshot, 
+		' en periodo: ', periodo);
+
+	for (var i=0; i<snapshot.length; i++){
+		var snapComercio = snapshot[i];
+		var idSnapComercio = snapComercio._id + '_' + periodo;
+		var record = {};
+		record.periodo = periodo;
+		record[idSnapshot] = snapComercio;
+		
+		console.log('Guardando snap en comercio/periodo: ', idSnapComercio);
+		
+		var insert = dbutils.update('sucursales_hist',
+			{ _id: idSnapComercio },
+			{
+				$set: record
+			},
+			{ upsert: true }
+		);
+
+		inserts.push(insert);
+	};
+
+	return Promise.all(inserts);
+
+}
